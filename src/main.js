58a508f220e5c049c04124c270df77fa066187a8
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-09-01 02:19:35 -0400

    v0.5 move discount rate up

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 // @author       jerryc05
 // @namespace    https://github.com/jerryc05
 // @supportURL   https://github.com/jerryc05/Weee-Helper
-// @version      0.4
+// @version      0.5
 // @description  Some Weee helpers
 // @match        https://sayweee.com/*
 // @match        https://*.sayweee.com/*
@@ -16,23 +16,21 @@
   'use strict'
 
   // show discount rate
-  for (const x of document.querySelectorAll('[class*="price"]')) {
-    const b = x.querySelector('[class*="basePrice"]')
-    if (b === null) continue
+  for (const x of document.querySelectorAll('[class*="resultItem_"]')) {
+    const b = x.querySelector('[class*="basePrice_"]')
+    if (!b) continue
     const p = parseFloat(x.querySelector('[class*="producsPrice"]').lastChild.textContent)
     const d = parseFloat(b.lastChild.textContent)
-    const s = document.createElement('span')
-    s.textContent = `-${((d - p) / d * 100).toFixed(0)}%`
-    s.style.marginLeft = '.4rem'
-    s.style.padding = '.1rem .4rem'
-    s.style.borderRadius = '1rem'
-    s.style.fontWeight = 'bold'
-    s.style.backgroundColor = 'red'
-    x.append(s)
+
+    const l = x?.querySelector('[class*="label_"]')
+    if (!l) continue
+    l.textContent = `${parseFloat(((d - p) / d * 100).toFixed(3))}% Off`
+    l.style.fontWeight = 'bold'
+    l.style.backgroundColor = 'red'
   }
 
   // show total amount in cart
-  const c = document.querySelector('[class*="miniCartInHeaderText"]')
+  const c = document.querySelector('[class*="miniCartInHeaderText_"]')
   if (c !== null) {
     const bearer = `Bearer ${document.cookie.match(/auth_token=([^;]+)/)[1]}`
     fetch('https://api.sayweee.net/ec/so/porder/v3', {
@@ -44,6 +42,6 @@
   }
 
   // remove refer text
-  const r = document.querySelector('[class*="referFriendText"]')
+  const r = document.querySelector('[class*="referFriendText_"]')
   if (r !== null) r.remove()
 })()
