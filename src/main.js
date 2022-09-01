d144507ca0051cba024fb85e8041e623c7e598a5
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-09-01 05:03:08 -0400

    v0.8 upd sort

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 // @author       jerryc05
 // @namespace    https://github.com/jerryc05
 // @supportURL   https://github.com/jerryc05/Weee-Helper
-// @version      0.7
+// @version      0.8
 // @description  Some Weee helpers
 // @match        https://sayweee.com/*
 // @match        https://*.sayweee.com/*
@@ -15,17 +15,20 @@
 (() => {
   'use strict'
 
+  function parsePrice(x) {
+    const p = parseFloat(x.querySelector('[class*="producsPrice"]').lastChild.textContent)
+    const b = x.querySelector('[class*="basePrice_"]')
+    return [p, b ? parseFloat(b.lastChild.textContent) : null]
+  }
+
   // show discount rate
   setTimeout(() => {
     for (const x of document.querySelectorAll('[class*="resultItem_"]')) {
-      const b = x.querySelector('[class*="basePrice_"]')
+      const [p, b] = parsePrice(x)
       if (!b) continue
-      const p = parseFloat(x.querySelector('[class*="producsPrice"]').lastChild.textContent)
-      const d = parseFloat(b.lastChild.textContent)
-
       const l = x?.querySelector('[class*="label_"]')
       if (!l) continue
-      l.textContent = `${parseFloat(((d - p) / d * 100).toFixed(3))}% Off`
+      l.textContent = `${parseFloat(((b - p) / b * 100).toFixed(3))}% Off`
       l.style.fontWeight = 'bold'
       l.style.backgroundColor = 'red'
     }
@@ -62,8 +65,22 @@
     const u = s.querySelector('ul')
     function f(u) {
       for (const x of ['Discount % CurPage', 'Discount $ CurPage']) {
-        const l = u.querySelector('li:last-child').cloneNode()
+        const l = u.querySelector('li:last-child').cloneNode(true)
         l.textContent = x
+        l.onclick = () => {
+          const items = document.querySelector('[class*="listContent_"]');
+          [...items.children]
+            .sort((a, b) => {
+              const [p1, b1] = parsePrice(a)
+              const [p2, b2] = parsePrice(b)
+              if (!b2) return 0
+              if (!b1) return 1
+              return x.includes('%')
+                ? ((p1 - b1) / p1) - ((p2 - b2) / p2)
+                : (p1 - b1) - (p2 - b2)
+            })
+            .forEach(x => items.appendChild(x))
+        }
         u.append(l)
       }
     }
