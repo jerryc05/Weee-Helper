ee1db88c5598b6fad038818f7a1057348d718838
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-09-01 01:45:02 -0400

    v0.3 show total amount next to cart

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@
 // @author       jerryc05
 // @namespace    https://github.com/jerryc05
 // @supportURL   https://github.com/jerryc05/Weee-Helper
-// @version      0.2
-// @description  Some Weee helper functionalities
+// @version      0.3
+// @description  Some Weee helpers
 // @match        https://sayweee.com/*
 // @match        https://*.sayweee.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sayweee.com
@@ -15,10 +15,10 @@
 (() => {
   'use strict'
 
-  for (const x of document.querySelectorAll('div[class*=\'price\']')) {
-    const b = x.querySelector('span[class*="basePrice"')
+  for (const x of document.querySelectorAll('div[class*="price"]')) {
+    const b = x.querySelector('span[class*="basePrice"]')
     if (b === null) continue
-    const p = parseFloat(x.querySelector('div[class*="producsPrice"').lastChild.textContent)
+    const p = parseFloat(x.querySelector('div[class*="producsPrice"]').lastChild.textContent)
     const d = parseFloat(b.lastChild.textContent)
     const s = document.createElement('span')
     s.textContent = `-${((d - p) / d * 100).toFixed(0)}%`
@@ -28,5 +28,16 @@
     s.style.fontWeight = 'bold'
     s.style.backgroundColor = 'red'
     x.append(s)
+  }
+
+  const c = document.querySelector('span[class*="miniCartInHeaderText"]')
+  if (c !== null) {
+    const bearer = `Bearer ${document.cookie.match(/auth_token=([^;]+)/)[1]}`
+    fetch('https://api.sayweee.net/ec/so/porder/v3', {
+      headers: { 'Authorization': bearer }
+    }).then(x => x.json())
+      .then(x => {
+        c.textContent += `  $${x.object.total_price}`
+      })
   }
 })()
